1719fc04cf80963eff0945af8fe6e02b3375892c
Entrar <Entrar@APD-L13>
2017-10-27 11:36:01 +0100

    Kata 4V2

--- src/kata4/Controlador/Kata4.java
+++ src/kata4/Controlador/Kata4.java
@@ -9,12 +9,31 @@ import kata4.Vista.MailListReader;
 
 public class Kata4 {
 
- public static void main(String[] args) throws IOException {
-    String filename = "Mail.txt";
-    List<String> mailList = MailListReader.read(filename);
-    Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-    HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-    histoDisplay.execute();
     
+
+    public static void main(String[] args) throws IOException, Exception {
+        Kata4 kata= new Kata4();
+        kata.execute();
+        
+    }
+    public Histogram<String> histogram;
+    public List<String> mailList;
+    
+    private void execute() throws Exception{
+        input();
+        process();
+        output();
+    }
+    private void process() throws Exception{
+        histogram = MailHistogramBuilder.build(mailList);
+        
+    }
+    private void input() throws IOException{
+        String filename = "Mail.txt";
+        mailList = MailListReader.read(filename);
+    }
+    private void output(){ 
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
 }
